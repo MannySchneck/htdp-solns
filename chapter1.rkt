b363844c70e8787cd6a74d8f7cd6d515648a1ab8
Manny Schneck <mannyschneck@gmail.com>
2016-01-21 15:19:41 -0600

    Add ex3

--- chapter1.rkt
+++ chapter1.rkt
@@ -4,15 +4,25 @@
 
 ; Exercise 1
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define x 3)
-(define y 4)
+;; (define x 3)
+;; (define y 4)
 
 ;; (sqrt (+ (* x x)
 ;;          (* y y)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Exercise 2
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define prefix "hello")
-(define suffix "world")
+;; (define prefix "hello")
+;; (define suffix "world")
 ;(string-append prefix "_" suffix)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Exercise 3
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (define str "helloworld")
+;; (define i 5)
+
+;; (string-append
+;;  (substring str 0 i)           ; takes substring from beginning to "o"
+;;  "_"                           ; the "_" to be inserted
+;;  (substring str i))            ; the rest of the string, from "o" to the end
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
